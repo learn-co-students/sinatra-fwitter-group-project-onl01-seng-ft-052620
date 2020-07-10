e25307ca34eda30689efaa7e55fbb0fcddaec6c9
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-10 18:35:52 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,58 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    redirect_to_if_not_logged_in
+    @tweets = Tweet.all
+    erb :'tweets/index'
+  end
+
+  get 'tweets/new' do
+    redirect_to_if_not_logged_in
+    erb :'tweets/new'
+  end
+
+  post '/tweets' do
+    tweet = current_user.tweets.build(params)
+    if tweet.save
+      redirect '/tweets'
+    else
+      redirect '/tweets/new'
+    end
+  end
+
+  get '/tweets/:id' do
+    redirect_to_if_not_logged_in
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'/tweets/show'
+  end
+
+  get '/tweets/:id/edit' do
+    redirect_to_if_not_logged_in
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'tweets/edit'
+  end
+
+  patch 'tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if @tweet.update(content: params[:content])
+      redirect '/tweets'
+    else
+      redirect "tweets/#{@tweet.id}/edit"
+    end
+  end
+
+  delete '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in? && current_user.owns_tweet?(@tweet)
+      @tweet.destroy
+    end
+      redirect '/tweets'
+  end
+
+  # private
+  #   def set_todo
+  #     @tweet = Tweet.find_by_id(params[:id])
+  #   end
+  # end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true
   has_secure_password
   has_many :tweets
+
+  def slug
+    self.username.downcase.split(" ")
+    .join("-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find { |user| user.slug == slug }
+  end
+
+  def owns_tweet?(tweet)
+    self.tweets.incluide?(tweet)
+  end
+
 end
