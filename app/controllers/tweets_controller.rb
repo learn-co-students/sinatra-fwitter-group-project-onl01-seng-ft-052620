af79c1db9f9f7eb05c55c851c28fb8f80ad21e67
Luca <lucakanyo@protonmail.com>
2020-07-22 19:51:06 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,69 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do 
+        redirect_to_if_not_logged_in
+        @tweets = Tweet.all
+        erb :'tweets/index'
+    end 
+
+    get '/tweets/new' do 
+        redirect_to_if_not_logged_in
+        erb :'/tweets/new'
+    end 
+
+    post '/tweets' do 
+        tweet = current_user.tweets.build(params)
+        if tweet.save 
+            redirect '/tweets'
+        else 
+            redirect '/tweets/new'
+        end 
+    end 
+
+
+get '/tweets/:id' do 
+    redirect_to_if_not_logged_in
+    set_tweet
+    erb :'tweets/show'
+end 
+
+get '/tweets/:id/edit' do
+    redirect_to_if_not_logged_in
+    set_tweet
+    if current_user.owns_tweet?(@tweet)
+    erb :'tweets/edit'
+    else 
+        redirect '/login'
+    end 
+end 
+
+patch '/tweets/:id' do 
+    @tweet = set_tweet
+    if current_user.owns_tweet?(@tweet)
+        @tweet.update(content: params[:content])
+        if @tweet.save
+            redirect '/tweets'
+        else 
+            redirect "/login"
+        end 
+    end 
+
+end 
+
+delete '/tweets/:id' do 
+    set_tweet 
+    if logged_in? && current_user.owns_tweet?(@tweet)
+        (@tweet) 
+        @tweet.destroy 
+        redirect '/tweets'
+    else 
+        redirect '/tweets'
+    end 
+end 
+
+private 
+    def set_tweet 
+        @tweet = Tweet.find_by_id(params[:id])
+    end 
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
 
+        get '/signup' do 
+            redirect_to_if_logged_in
+            erb :'users/signup'
+        end 
+
+        post '/signup' do 
+            user = User.new(params)
+            if user.save 
+                login_user(user)
+                redirect '/tweets'
+            else 
+                redirect '/signup'
+            end 
+        end 
+
+        get "/users/:slug" do 
+            @user = User.find_by_slug(params[:slug])
+            erb :'users/show'
+        end 
 
 end
